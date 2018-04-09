259294e161d4de552474de4e0d2ad038c882f3fd
d0ku <darthtyranus666666@gmail.com>
2018-04-09 22:33:01 +0200

    Backup dotfiles before linking to new ones.

--- initial_config/link_dotfiles.sh
+++ initial_config/link_dotfiles.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+mkdir ~/backup_dotfiles
+mv ~/.vimrc ~/backup_dotfiles
+mv ~/.xinitrc ~/backup_dotfiles
+mv ~/.Xdefaults ~/backup_dotfiles
+
+echo "Your original files are backed up in ~/backup_dotfiles folder."
+
 ln -f ~/.config/i3/initial_config/dotfiles/.vimrc ~/.vimrc 
 ln -f ~/.config/i3/initial_config/dotfiles/.Xdefaults ~/.Xdefaults 
 ln -f ~/.config/i3/initial_config/dotfiles/.xinitrc ~/.xinitrc 
