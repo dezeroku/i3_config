d435126d0ca60df5a77b2b98f52bf8f77b30f808
d0ku <darthtyranus666666@gmail.com>
2018-05-09 11:17:23 +0200

    added go doc package

--- initial_config/go_packages.sh
+++ initial_config/go_packages.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Basic tutorial, it might come in handy.
 go get golang.org/x/tour/gotour
+# Browse docs.
+go get golang.org/x/tools/cmd/godoc
 
 go get -u github.com/nsf/gocode
 go get -u golang.org/x/lint/golint
