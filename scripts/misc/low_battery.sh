84e2facb5180af1823da123f97f3aaa32fa44e91
d0ku <darthtyranus666666@gmail.com>
2018-11-19 21:31:35 +0100

    upd

--- scripts/misc/low_battery.sh
+++ scripts/misc/low_battery.sh
@@ -6,8 +6,8 @@
 # You can adjust low_battery value to display notification basing on the battery level.
 low_battery=20;
 
-percentage=$(acpi -b | awk '{print substr($4,0,2)}')
+percentage=$(acpi -b | grep -P -o '[0-9]+(?=%)')
 
 if [ $percentage -lt $low_battery ] && [ $percentage -ne 10 ]; then
-    notify-send "Low battery" "You only got $percentage% left";
+     DISPLAY=:0.0 /usr/bin/notify-send "Low battery" "You only got $percentage% left";
 fi;
